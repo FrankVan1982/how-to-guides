a415d901859f4a92eaaa8ab18a5b620d3744859d
FraV1982 <consulenza.vangi@outlook.com>
2024-07-18 17:14:56 +0200

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
   socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind',
-    mastodon: '@atinux@webtoo.ls'
+    twitter: 'FraV1982',
+    github: 'FrankVan1982/how-to-guides',
+    mastodon: ''
   }
 })
